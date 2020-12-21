72af19def84f59c8bd100089e0b19c4d821e65e2
osteophagy <mnovakahs@gmail.com>
2020-12-21 15:09:43 -0500

    got document appending textareas. deciding on logic for spawning all 8 hrs + eventlisteners for time change

--- logic.js
+++ logic.js
@@ -1,5 +1,21 @@
-//call out variables for stuff that will be called throughout the document
-let schedule = $(".SCHEDULE-BLOCK") //container content will be populated to
+//time -- can't decide whether this+date should be global or not, other stuff might read them?
+let currentTime = $(".TIME")
+setInterval(function(){
+    currentTime.empty()
+    currentTime.append(dayjs().format("h:mm:ss a"))
+},1000)
+
+//date
+let currentDate = $(".DATE")
+setInterval(function(){
+    currentDate.empty()
+    currentDate.append(dayjs().format("ddd . MM . DD"))
+},1000)
+
+//schedule 
+let schedule = $(".SCHEDULE") //container content will be populated to
+let workHours = $(".HOURS") //container for hours
+
 let workDay = [
     {time:"9am"},
     {time:"10am"},
@@ -12,16 +28,16 @@ let workDay = [
     {time:"5pm"},
 ]
 
-//load in all content for page 
-//ev is a random placeholder variable, short for 'event'
+//Populate workHours with hours of the day
 
-function buildPage(ev){
-    schedule.empty();
-    for (i=0; i < ev.length; i++){
-        let row = $()
-    }
+//Populate scheduleItems with textareas
+//can i set this to spawn in exactly 8? 1 per hour
+//test just getting one spawned in before we mess with other stuff
+function appendTasks(){
+    let tasks = $("<textarea>")
+    tasks.attr("id", "textarea")
+    $(".tasks").append("<textarea>")
 }
 
-// if I want the time element to be decorated differently:  
-//(items stack vertically in a skinny black div)
-// then I'm going to need to have the document populate that content to a different div...that should be okay
+//part of the initialization of the document, move higher into a function that calls all these guys?
+appendTasks()
