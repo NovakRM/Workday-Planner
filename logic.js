bef4166f75c0a254c49248720bd5963a20c5be90
osteophagy <mnovakahs@gmail.com>
2021-01-02 14:36:59 -0500

    changing localstorage structure a bit, listeners on all items but only storing the first textarea for some reason

--- logic.js
+++ logic.js
@@ -37,61 +37,71 @@ let workDay = [
 
 
 buildSchedule(workDay) //parsing in workDay is what specifies the array for text to be pulled from
-saveItems(workDay)
+//saveItems(workDay)
 
 function buildSchedule(workDay){
     for (i=0; i<workDay.length; i++){
-        //Populate workHours with hours of the day [DONE!]
-        let container =  $("<div></div>").addClass("row")
+        
+        let container =  $("<div></div>").addClass("container row") 
+        container.attr("key", workDay[i].key) //using container class and its key now to compare for times in color function below
+
+        //Populate container with hours of the day [DONE!]
         let hour =  $("<div></div>").addClass("hour col-2").text(workDay[i].time) // create a div for each hour, apply class hour, textcontent is pulled from time index 
         container.append(hour)// append to main container 
 
-        //Populate scheduleItems with textareas [DONE!]
+        //Populate container with textareas [DONE!]
         let tasks = $("<textarea></textarea>")
         tasks.attr("class", "textbox col-8")
-        tasks.attr("id", "foo" + workDay[i].key)
-        tasks.attr("key", workDay[i].key)
+        tasks.attr("id", "foo") //id to point to in storage function
+        tasks.val(localStorage.getItem(workDay[i].key))
+
         container.append(tasks)
 
-        //Add Buttons [DONE]
+        //Populate container with buttons [DONE]
         let buttons = $("<button></button>").addClass("fas fa-calendar-check fa-2x col-2")
-        buttons.attr("id", "bar" + workDay[i].key)
+        buttons.attr("id", "bar") //id to point to in storage function
         container.append(buttons)
         
         //Append all of the above to document [DONE]
         $(".SCHEDULE").append(container)
-
     }
 }
 
 
 //Set local storage [TESTING]
-function saveItems (workDay){
-    for (i=0; i<workDay.length; i++){ //looping thru workDay array
-        $("#" + "bar" + workDay[i].key).on("click", function(){ //grabbing previously appended buttons
-            console.log("bar")
-            let userInput = $("#foo" + workDay[0].key + "")
-            localStorage.setItem("foo" + workDay[0].key , userInput[0].value)
-        })
+$(document).on("click", function(event){ //listener to entire document
+    if (event.target.matches("#bar")){ //event delegation for if click target = BAR button
+        let value = $("#bar").siblings("#foo").val() //elements in same row, point to sibling textarea
+        let key = $("#bar").parent().attr("key") //pointing to key of parent element, row
+        console.log(key,value)
+        localStorage.setItem(key,value)
     }
-}
+})
 
-//Local Storage items [TESTING]
-foo9.value = localStorage.getItem("foo9")
-foo10.value = localStorage.getItem("foo10")
-foo11.value = localStorage.getItem("foo11")
-foo12.value = localStorage.getItem("foo12")
-foo13.value = localStorage.getItem("foo13")
-foo14.value = localStorage.getItem("foo14")
-foo15.value = localStorage.getItem("foo15")
-foo16.value = localStorage.getItem("foo16")
-foo17.value = localStorage.getItem("foo17")
+// $("#bar").on("click", function(){
+//     var value = $(this).siblings("#foo").val();
+//     var key =  $(this).parent().attr("key")
+//     console.log(key, value);
+
+//     localStorage.setItem(key, value)
+// })
+
+// //Local Storage items [TESTING]
+// foo9.value = localStorage.getItem("foo9")
+// foo10.value = localStorage.getItem("foo10")
+// foo11.value = localStorage.getItem("foo11")
+// foo12.value = localStorage.getItem("foo12")
+// foo13.value = localStorage.getItem("foo13")
+// foo14.value = localStorage.getItem("foo14")
+// foo15.value = localStorage.getItem("foo15")
+// foo16.value = localStorage.getItem("foo16")
+// foo17.value = localStorage.getItem("foo17")
 
 
 //if/else statement comparing the time listed in .hour div to the current local time [DONE]
 function setColor(){
     for (let i=0; i<9; i++){
-        let newColor = $(".textbox")[i].attributes[2].value // pointing to key value assigned to each textarea
+        let newColor = $(".container")[i].attributes[1].value // pointing to key value assigned to each textarea
         if(parseInt(newColor) === parseInt(dayjs().format('H'))){
             $(".textbox")[i].className="textbox col-8 present"
         }else if(parseInt(newColor) < parseInt(dayjs().format('H'))){
